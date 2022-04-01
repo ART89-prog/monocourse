e028c41588cd1bd9bd90b5060001c87b357b7c0a
ART89-prog <72185684+ART89-prog@users.noreply.github.com>
2022-04-02 00:17:37 +0300

    Мобильная версия

--- js/scripts2.js
+++ js/scripts2.js
@@ -20,6 +20,55 @@ $(() => {
 	})
 
 
+	$('body').on('click', '.search-result_item button', function(e) {
+		$(this).parent().hide();
+		let id = $(this).parent().data("id");
+		$("input[id='"+id+"']").prop("checked", false);
+		if($(".search-result .search-result_item:visible").length==0)
+		{
+			$(".search-result .reset").hide();
+		}
+	});
+
+	$(".search-result .reset").on('click', function(e) {
+		$(this).hide();
+		$(".search-result_item").hide();
+		$(".filter").trigger("reset");
+	});
+
+
+	$(".filter input").on("change", function(){	
+		if($(this).prop("checked"))
+		{
+			let name = $(this).next().text();
+			if(name=="")
+			{
+				name = $(this).prev().text();
+			}
+
+			$(".search-result .reset").before('<div class="search-result_item" data-id="'+$(this).prop("id")+'"><span>'+name+'</span><button><img src="images/delete.png" alt=""></button></div>');
+			$(".search-result .reset").show();
+		}	
+		else
+		{
+			let id = $(this).prop("id");
+			console.log(".search-result_item[data-id='"+id+"']");
+			$(".search-result_item[data-id='"+id+"']").hide();
+			if($(".search-result .search-result_item:visible").length==0)
+			{
+				$(".search-result .reset").hide();
+			}
+
+		}
+	});
+
+	$(".show-more").on('click', function(e) {
+		e.preventDefault()
+		$(this).prev().find(".hide").show();
+		$(this).hide();
+	});
+
+
 	$('select').niceSelect();
 
 
@@ -37,8 +86,6 @@ $(() => {
 		}
 
 	})
-
-
 
 
 })
